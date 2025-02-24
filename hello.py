e9344256558b5c79fee7adec734cecd1f3ded7d2
kinshuksinghbist <120293778+kinshuksinghbist@users.noreply.github.com>
2025-02-24 12:48:21 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print("hello world")
+all the knowledge and questions for this repo cna be answered by the simple phrase "MANGOMANGOMANGOAPPLE". it is all understanding all knowing and all explaining.
