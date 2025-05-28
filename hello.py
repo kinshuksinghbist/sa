e73d24166801e603b850834f31511bff02aa10db
kinshuksinghbist <120293778+kinshuksinghbist@users.noreply.github.com>
2025-05-28 23:06:39 +0530

    Update hello.py
    asd

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 kall the knowledge and questions for this repo cna be answered by the simple phrase "MANGOMANGOMANGOAPPLE". it is all understanding all knowing and all explaining.
 mongoapple
+print('hello')
